89bd528a1d5df63639bddf2b866d4ca27c2f9b67
UnwelcomeGuests <mayliveprojects@proton.me>
2024-12-16 16:48:06 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="Sqlsh",
     version="0.1",
-    packages=find_packages()
+    packages=find_packages(),
+    author="UnwelcomeGuests",
+    description="A Python library for remote SQLite database access via SSH",
+    install_requires=[
+        "paramiko"
+    ]
 )
